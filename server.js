f171f3dd435d37cbfab683fafb8b87982ed793eb
Jack Barker <jack.barker.au@gmail.com>
2015-02-02 22:48:39 +1100

    up to video 5.5 (inclusive)

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+    passport = require('passport'),
+    LocalStrategy = require ('passport-local').Strategy;
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -11,6 +13,33 @@ require('./server/config/express')(app, config);
 require('./server/config/mongoose')(config);
 
 //require('./server/config/passport')();
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.findOne({username:username}).exec(function(err, user){
+      if(user){
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+  }
+));
+
+passport.serializeUser(function(user, done){
+  if(user){
+    done(null, user._id);
+  }
+});
+
+passport.deserializeUser(function(id, done){
+  user.findOne({_id:id}).exec(function(err,user){
+    if(user){
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  })
+});
 
 require('./server/config/routes')(app);
 
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,21 @@ module.exports = function(config){
   db.once('open', function callback() {
     console.log('db connection opened');
   });
+
+  var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    userName: String
+  });
+  var User = mongoose.model('User', userSchema);
+
+  User.find({}).exec(function (err, collection){
+    if(collection.length === 0){
+      User.create({firstName: 'Jack', lastName: 'Barker', userName: 'jackb'});
+      User.create({firstName: 'Tom',  lastName: 'Jones', userName: 'tom'});
+      User.create({firstName: 'Jane', lastName: 'Doe', userName: 'dan'});
+    }
+  })
+
+
 }
